977ae1c8be96acf681f0271c36f21c9c0e5d76e9
adorableqnhu <142926468+adorableqnhu@users.noreply.github.com>
2024-08-21 18:10:45 +0700

    audio
    add audio

--- index.js
+++ index.js
@@ -1,59 +1,20 @@
-const contentLetterSrart_actived = "Hi! Chỉ là vài lời chúc nho nhỏ~Click vào hộp quà ~ ~" //Lời mở đầu cho bức thư
+const contentLetterSrart_actived = "Happy Birthday =))))) Nội dung chắc vẫn như cũ thôi nhưng đó là những lời chúc chân thành nhất rồi đó （づ￣3￣）づ╭❤️～" //Lời mở đầu cho bức thư
 var mainContentLetter = "Mong rằng tuổi mới sẽ đầy hạnh phúc, thành công và niềm vui. >" +
-"Tuổi mới hy vọng bạn sẽ gặp được nhiều người mới đối xử tốt với bản thân bạn. Không cần gì quá nổi bật, chỉ cần họ tôn trọng bạn là được. >" +
+"Tuổi mới bạn sẽ gặp được nhiều người mới đối xử tốt với bản thân bạn. Không cần gì quá nổi bật, chỉ cần họ tôn trọng bạn là được. >" +
 "Mong bạn luôn vui vẻ, mọi vấn đề đều xem là nhẹ và luôn giải quyết được tất cả. >" +
 "Mọi giấc ngủ đều là giấc ngủ ngon, không muộn phiền và những giấc mơ sẽ chữa lành mọi phiền não, nổi đau của bạn. >" +
 "Mỗi năm qua đi, bạn càng trở nên xinh đẹp và mạnh mẽ hơn. Hãy tiếp tục là phiên bản tốt đẹp nhất của chính mình! >" +
 "Hãy để những ngày tươi đẹp đánh dấu cuộc hành trình tiếp theo của bạn. Hãy biến mỗi khoảnh khắc thành một kỷ niệm đáng nhớ. >" +
-"Chúc mỗi ngày đều có ý nghĩa để khi nhìn lại bạn không phải hối tiếc bất kỳ điều gì. ~"
-// Nếu có cơ may bạn đọc những dòng này, thì chắc có lẽ bạn đã tìm được người yêu rồi nhĩ? 
-// Thật ra nếu bạn nhớ lại thì đã từng có 1 khoảng thời gian tui đổi cách nch cũng như cách đối xử với bạn, không phải vì tui bất mãn với điều gì từ bạn đâu.
-// Mà chỉ đơn giản là... hmm hình như lúc bấy giờ, tui đã thích bạn mất rồi.
-// Không phải với tư cách là những người bạn thích chơi với nhau, mà là sự ích kỉ chỉ muốn bạn thuộc về mình thôi.
+"Chúc mỗi ngày đều có ý nghĩa để khi nhìn lại bạn không phải hối tiếc bất kỳ điều gì. >" +
+"~ Have a wonderful trip ~"
 
-// Có điều này mình tự tin đảm bảo rằng mình không phải thích bạn từ vẻ đẹp bên ngoài của hiện tại.
-// Mình đã chơi với nhau từ thuở xưa, dù cao, ốm, xấu, mập hay dù bất kì điều gì đi nữa mình vẫn chơi với nhau đến tận ngày nay mà.
-// Mình không phải thích bạn do bạn đã trở nên xinh đẹp hơn xưa, tài năng hơn, đảm đang và nhiều nhiều thứ tích cực hơn xưa...
-// Chắc là do có 1 khoảng thời gian mình tiếp xúc gần với nhau nhĩ? Ngồi cạnh bên hiểu thói quen sinh hoạt của nhau,
-// được chăm sóc bạn từ chính đôi bàn tay này.. có thể bạn không biết mình đã vui đến mức nào khi được chăm sóc bạn vài ngày đó đâu.
-// Thích những lúc bạn làm nũng với mình, thật sự rất dễ thương.. dễ thương đến mức mình đã mất kiểm soát bản thân.
-// Thích cách ứng xử, thích giọng nói, thích những lời đùa giỡn, thích những lần tâm sự trên chiếc xe chạy vô định giữa lòng Sài Gòn.
-// Hay chỉ đơn giản, mình thích bạn.
-// Nhìn bạn cứ kiểu bị nhỏ bé ấy, làm bản thân muốn che chở bảo vệ cho bạn. Nhưng mình đã nhận ra rằng bạn hoàn toàn không cần những điều đó.
-// Bạn bản lĩnh, cứng rắn, mạnh mẽ hơn bất kì ai. Và mình tin rằng chẳng có chuyện gì có thể làm khó được bạn.
+// Biết là sẽ không đọc tới đây đâu, nhưng lỡ đâu có ai giúp để đọc được những lời này thì coi như 1 "Easter Egg" của web đi ha :D
+// Tui không quan tâm kết quả có tốt đẹp hay không nhưng trong khoảng thời gian đó tui đã phải đổi tiếng thông báo messenger riêng với bạn.
+// Bạn chỉ mất đi một thằng thất bại và yêu Như.
+// Tui biết là bạn rất giỏi, đủ tự lập và mạnh mẽ để không cần dựa dẫm vào ai nhưng mà tui chỉ muốn bạn biết thêm là hễ có chuyện gì xảy ra thì có tui got your back.
 
-// Mình cần xác nhận lại tư tưởng của mình, mình đã đi quá xa rồi và mong rằng bạn tha lỗi cho những việc vượt qua tý ranh giới của giới hạn tình bạn.
-// Nghĩ cũng nhiều rồi, có lẽ chúng ta thật sự không hợp cho 1 mqh yêu đương thật. Có lẽ chúng ta quá giống nhau,
-// hiểu được rõ đối phương đang ám chỉ điều gì hay vì sao hành xử như vậy. Ví dụ cụ thể nhé?:
-// Ngày bạn hẹn mình thứ 5 đi thay mắt kính & đi ăn, cái ngày mà bạn cho mình leo cây dù mình hẹn nhau từ tận CN tuần trước ấy,
-// bạn có bao giờ nghĩ thật sự mình không hiểu gì để bạn dắt như thế không?
-// Mình biết rõ, vì mình chẳng thể nào được xếp vào vị trí ưu tiên cả đúng không? Không thể nào có người bận đến mức không thể nhắn 1 tin nhắn cả,
-// dù là vừa hẹn ngay lúc tan làm đi chơi, thì quãng đường từ văn phòng tới bãi giữ xe thôi, chỉ 1 tin nhắn thôi, không lẽ không làm được.
-// Chỉ là vì bạn quên mất đúng không ? Không phải mình kể công hay nhỏ nhen chỉ vì hủy hẹn giờ chót đâu, lúc đó mình đã thật lòng nói với bạn rồi.
-// Hôm đó dù rất rất là mệt, nhưng lúc đó cũng đã 6h rồi, mình đã đợi tin nhắn bạn từ sáng cơ. Tận 6h mình vẫn chưa nhận được tin nào về buổi chiều đó.
-// Mình chỉ dám hẹn 10p để ngủ thôi, và ngủ đúng 3p thì bạn gọi cho mình "alo" sau đó thì...
-// Bạn nghĩ làm sao vừa hẹn giờ mà ngủ ngay được ? Thật sự mình đã mệt tới mức độ vừa hẹn xong là bất tỉnh đấy. Lúc đó,
-// mình chỉ sợ rằng bạn có chuyện gì thôi, biết bao nhiêu viễn cảnh mà mình có thể vẽ ra trong đầu nó kinh khủng lắm.
-// Mà chắc là do đã thích bạn rồi nên tật xấu đó không thể bỏ. Lúc bạn gọi lại, mình đứng trước cửa trọ mình mừng lắm, bạn không sao.
-
-// Ví dụ 2 nhé?: 
-// Hôm chủ nhật mình có hẹn qua ăn lẩu chay ấy? Vẫn là kịch bản cũ, dù mình đã hẹn nhau từ trước tận 2-3 ngày gì cơ,
-// Nhưng đến cuối cùng, trưa hôm ấy có tin nhắn là bạn đã bỏ ngang câu chuyện mình đã bàn với nhau từ trước.
-// 1 lần thì có thể là trùng hợp, 2 lần thì phải nghi ngờ đúng không? Mình đã nghi rằng mình chẳng thể bằng những người bên cạnh bạn hiện tại dù có là ai.
-
-// Và... lần thứ 3 nữa thì là KHẲNG ĐỊNH rồi còn gì bạn nhĩ? 
-// Vậy nên, đã đến lúc tui phải từ bỏ việc muốn bạn là của riêng bản thân rồi. Bản thân đã không quan trọng trong lòng người khác thì dù có cố gắng
-// cách mấy cũng không thể nào phát triển hơn được.
-
-// Thật may vì bên cạnh bạn giờ đây đã có những người luôn sẵn sàng đồng hành cùng bạn, cùng tầng lớp và hiểu bạn hơn mình.
-// Chúc bạn luôn vui vẻ, không bao giờ cô đơn nữa. Chúc bạn thật nhiều sức khỏe để có thể tận hưởng hết mình tuổi trẻ này.
-
-// Cảm ơn vì quãng thời gian vui vẻ trong sự ngộ nhận của chính bản thân tui. 
-// Cảm ơn vì những giấc mơ thật đẹp mà bạn mang lại cho mình.
-// Cảm ơn vì đã tha thứ cho những lỗi lầm.
-// Cảm ơn vì tất cả.
-
-// Từ giờ, tui sẽ cố gắng đối xử với bạn như 1 người bạn thật bình thường và trong sáng, sẽ luôn ủng hộ bạn bất kể chuyện gì.
+// If I could give you one thing in this life, I would give you the ability to see yourself through my eyes. Only then, would you realize how special you are to me.
+// Btw~ I Love you.
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
@@ -62,6 +23,11 @@ imgStart.src = "./img/Subject.png";
 // Gắn 1 link ảnh bất kì
 let imgLetter = document.querySelector(".img");
 imgLetter.src = "./img/zzz1.jpg"; //Hình ảnh xuất hiện trong nội dung của bức thư sau khi bức thư được viết ra hết
+
+function playAudio() {
+    var audio = document.getElementById('myAudio');
+    audio.play();
+}
 
 const splitContentLetterSrart_actived = contentLetterSrart_actived.split("");
 
